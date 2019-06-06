11fee5df9fac48956e0cef29e70b268732cad67f
Peter Soboyejo <thepcmrtim@gmail.com>
2019-06-06 09:46:50 -0400

    this is one big mess

---
+++ precache-manifest.5c178f2823b4d84ff460e9b01cf06661.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4507be595a97a0374cc18c08b61e5a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a3a16b6f6214c0b4270",
+    "url": "/static/css/main.1c9a8808.chunk.css"
+  },
+  {
+    "revision": "fa21bccc94acbbbefab7",
+    "url": "/static/js/2.56e7ac16.chunk.js"
+  },
+  {
+    "revision": "4a3a16b6f6214c0b4270",
+    "url": "/static/js/main.8545a4d4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "818631e414d8b74064ed6fc92ca37783",
+    "url": "/static/media/algonquin.818631e4.png"
+  },
+  {
+    "revision": "8ee8690501624e790069a3df014d814a",
+    "url": "/static/media/kleidi.8ee86905.png"
+  },
+  {
+    "revision": "cecbf7c4a74ad3ba598072b87c877c63",
+    "url": "/static/media/sscout.cecbf7c4.png"
+  }
+]);
